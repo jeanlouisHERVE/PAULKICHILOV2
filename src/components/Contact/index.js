09edf6c3c68aae8e8824418572f1c7bfd06ba4aa
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-09-28 09:14:29 +0200

    mise en page de la page contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,9 +5,19 @@ const Contact = () => {
   const cssClass = 0;
 
   return (
-    <main className={cssClass}>
-    <DivHiddenUnderNavbar />
-      <h1 className="posts-title">Contact</h1>
+    <main className="contact__container">
+      <DivHiddenUnderNavbar />
+      <div className="contact__picture--under--navbar"></div>
+      <div className="contact__under--navbar">
+        <div className="contact__left">
+          <h1 className="posts-title">Contact</h1>
+          <p>Paul KICHILOV</p>
+          <p>Email : <a href="mailto:paul.kichilov@gmail.com?subject=Renseignements">paul.kichilov@gmail.com</a></p> 
+          <p>Tél : <a href="tel:+330760457615">07.60.45.76.15</a></p>
+        </div>
+        <div className="contact__right">
+        </div>
+      </div>   
     </main>
   );
 };
